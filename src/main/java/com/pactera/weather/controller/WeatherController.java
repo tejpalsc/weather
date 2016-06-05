bf987825fb80c32e78edbf7a6c8cfcec6a7529da
Tejpal Chhabra <tejpal.chhabra@gmail.com>
2016-06-05 17:47:49 +1000

    Added UI and services.. WIP

--- src/main/java/com/pactera/weather/controller/WeatherController.java
+++ src/main/java/com/pactera/weather/controller/WeatherController.java
@@ -2,6 +2,7 @@ package com.pactera.weather.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,19 +13,23 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.pactera.weather.exception.ApplicationException;
 import com.pactera.weather.exception.ValidationException;
 import com.pactera.weather.model.WeatherDetails;
+import com.pactera.weather.service.WeatherDetailsService;
 
 @Controller
 public class WeatherController {
 
 	private static final Logger logger = LoggerFactory
 			.getLogger(WeatherController.class);
+	
+	@Autowired
+	private WeatherDetailsService weatherService;
 
 	@RequestMapping(value = "/details", method = RequestMethod.GET)
 	public @ResponseBody
 	WeatherDetails getWeatherDetails(@RequestParam String city) throws ValidationException {
 		logger.debug("Serving weather details for city: {}.", city);
 		
-		return new WeatherDetails();
+		return weatherService.getDetails(city);
 	}
 	
 	@RequestMapping(value = "/cities", method = RequestMethod.GET)
--- src/test/java/com/pactera/weather/controller/WeatherControllerTest.java
+++ src/test/java/com/pactera/weather/controller/WeatherControllerTest.java
@@ -7,16 +7,27 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import com.pactera.weather.model.WeatherDetails;
+import com.pactera.weather.service.LocationService;
+import com.pactera.weather.service.WeatherDetailsService;
+
 @ContextConfiguration("/test-context.xml")
 @RunWith(SpringJUnit4ClassRunner.class)
 public class WeatherControllerTest {
 		
+    @Mock
+    WeatherDetailsService weatherService;
+    
+    @Mock
+    LocationService locationService;
 
     @InjectMocks
     WeatherController weatherController;
@@ -31,13 +42,18 @@ public class WeatherControllerTest {
 	
 	@Test
 	public void testWeatherDetails() throws Exception {
-;
+		String[] supportedCities = new String[]{"Sydney"};
+		Mockito.when(locationService.getSupportedCities()).thenReturn(supportedCities);
+		WeatherDetails wd = new WeatherDetails();
+	    Mockito.when(weatherService.getDetails("Sydney")).thenReturn(wd);
 	    mockMvc.perform(get("/details?city=Sydney")).andExpect(status().is2xxSuccessful());
 	}
 	
 	
 	@Test
 	public void testLocation() throws Exception {
+		String[] supportedCities = new String[]{"Sydney"};
+		Mockito.when(locationService.getSupportedCities()).thenReturn(supportedCities);
 		mockMvc.perform(get("/cities")).andExpect(status().isOk());
 	}
 	
